aa253ccc4623bed99bd1fcedaa289d4ecf4831c4
joshuaalbert <albert@strw.leidenuniv.nl>
2023-06-16 01:35:46 +0200

    * bump to 1.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='fair_async_rlock',
-      version='1.0.2',
+      version='1.0.3',
       description='A well-tested implementation of a fair asynchronous RLock for concurrent programming.',
       long_description=long_description,
       long_description_content_type="text/markdown",
