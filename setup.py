02bf4829f3346c5a5f0d586255ce78000ff36c9e
Jaagup Averin <jaagup.averin@gmail.com>
2024-05-12 18:12:06 +0300

    Add AnyIO and type-hints; other modernization.
    * Rewrite FairAsyncRLock to use AnyIO so it can be used with asyncio, trio, etc.
    * Rewrite tests using AnyIO API
    * Add [project] to pyproject.toml, as expected by some modern packaging tools (e.g Rye)
    * Update requirements.txt as needed by AnyIO
    * Add project classifiers
    * Bump Python requirement to >=3.11
    * Bump project version to 1.1.0

--- setup.py
+++ setup.py
@@ -1,31 +1,47 @@
 #!/usr/bin/env python
 
-from setuptools import find_packages
+import os
+
 from setuptools import setup
 
-with open("README.md", "r") as fh:
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = f"{lib_folder}/requirements.txt"
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
+tests_requirement_path = f"{lib_folder}/requirements-tests.txt"
+tests_require = []
+if os.path.isfile(tests_requirement_path):
+    with open(tests_requirement_path) as f:
+        tests_require = f.read().splitlines()
+
+with open("README.md") as fh:
     long_description = fh.read()
 
-setup(name='fair_async_rlock',
-      version='1.0.7',
-      description='A well-tested implementation of a fair asynchronous RLock for concurrent programming.',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url="https://github.com/Joshuaalbert/FairAsyncRLock",
-      author='Joshua G. Albert',
-      author_email='albert@strw.leidenuniv.nl',
-      setup_requires=[],
-      install_requires=[],
-      tests_require=[
-          'pytest',
-          'pytest-asyncio'
-      ],
-      package_dir={'': './'},
-      packages=find_packages('./'),
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: Apache Software License",
-          "Operating System :: OS Independent",
-      ],
-      python_requires='>=3.7',
-      )
+setup(
+    name="fair_async_rlock",
+    version="1.1.0",
+    description="A well-tested implementation of a fair asynchronous RLock for concurrent programming.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Joshuaalbert/FairAsyncRLock",
+    author="Joshua G. Albert",
+    author_email="albert@strw.leidenuniv.nl",
+    setup_requires=[],
+    install_requires=install_requires,
+    tests_require=tests_require,
+    package_dir={"fair_async_rlock": "./fair_async_rlock"},
+    packages=["fair_async_rlock"],
+    package_data={"fair_async_rlock": ["py.typed"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Framework :: AnyIO",
+        "Typing :: Typed",
+    ],
+    python_requires=">=3.11",
+)
